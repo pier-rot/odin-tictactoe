44e355e59ae858b86c77b37dbea2d91a2fde6bf4
Pierre V <6772696d@gmail.com>
2024-09-03 09:33:38 +0200

    Make Gameboard object

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function Cell() {
     let value = 0;
     const getValue = () => value;
-    const setTokenFromPlayer = (player) => {value = player.getID();}
+    const setValueFromPlayer = (player) => {value = player.getID();}
 
     return {
         getValue,
-        setTokenFromPlayer
+        setValueFromPlayer
     }
 }
 
@@ -23,5 +23,34 @@ function Player(name, token, ID) {
 }
 
 function Gameboard() {
+    let rows = 3;
+    let columns = 3;
+    let board = [];
 
+    for(let i = 0; i<rows; i++){
+        board[i] = [];
+        for(let j = 0; j<columns; j++) {
+            board[i].push(Cell());
+        }
+    }
+
+    const getBoard = () => {board;};
+
+    const putPlayerAt = (player, row, column) => {
+        if (board[row][column] === 0) {
+            board[row][column] = player.getID();
+        } else {
+            return;
+        }
+    };
+
+    const printBoard = () => {
+        console.log(getBoard());
+    };
+
+    return {
+        getBoard,
+        putPlayerAt,
+        printBoard
+    }
 }
